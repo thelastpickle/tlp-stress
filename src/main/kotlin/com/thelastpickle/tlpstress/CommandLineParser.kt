d7a31d83faf8cfbd1905a5e8d9c972937d1d5bb7
Jon Haddad <jon@jonhaddad.com>
2018-08-08 17:56:09 -0700

    Can now get help using -h, like a normal program.  Closes #20

--- src/main/kotlin/com/thelastpickle/tlpstress/CommandLineParser.kt
+++ src/main/kotlin/com/thelastpickle/tlpstress/CommandLineParser.kt
@@ -1,19 +1,32 @@
 package com.thelastpickle.tlpstress
 
 import com.beust.jcommander.JCommander
+import com.beust.jcommander.Parameter
 import com.thelastpickle.tlpstress.commands.IStressCommand
 import com.thelastpickle.tlpstress.commands.Info
 import com.thelastpickle.tlpstress.commands.Run
 
+class MainArgs {
+
+    @Parameter(names = ["--help", "-h"], description = "Shows this help.")
+    var help = false
+
+}
 
 class CommandLineParser(val jCommander: JCommander,
                         val commands: Map<String, IStressCommand>) {
+
 
     companion object {
         fun parse(arguments: Array<String>): CommandLineParser {
 
             // JCommander set up
-            val jcommander = JCommander.newBuilder()
+            val jcommander = JCommander.newBuilder().programName("tlp-stress")
+            val args = MainArgs()
+
+            // needed to get help
+            jcommander.addObject(args)
+
 
             // subcommands
             val commands = mapOf(
