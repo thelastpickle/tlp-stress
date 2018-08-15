7b59f83eefcb5cf409991a6aa79ce8ee58b4ffbb
Jon Haddad <jon@jonhaddad.com>
2018-08-14 17:34:20 -0700

    minor code cleanup

--- src/main/kotlin/com/thelastpickle/tlpstress/Metrics.kt
+++ src/main/kotlin/com/thelastpickle/tlpstress/Metrics.kt
@@ -6,23 +6,14 @@ import java.util.concurrent.TimeUnit
 class Metrics {
     val metrics = MetricRegistry()
 
-//    val reporter = ConsoleReporter.forRegistry(metrics)
-//            .convertRatesTo(TimeUnit.SECONDS)
-//            .convertDurationsTo(TimeUnit.MILLISECONDS)
-//            .build()
-
     val reporter = SingleLineConsoleReporter(metrics)
-
-
 
     fun startReporting() {
         reporter.start(1, TimeUnit.SECONDS)
-//        reporter2.start(1, TimeUnit.SECONDS)
     }
 
     val errors = metrics.meter("errors")
     val mutations = metrics.timer("mutations")
     val selects = metrics.timer("selects")
 
-//    val requestTimer = metrics.timer("requestTimer")
 }
