d494cb4b8b3c0be146a9d5c37f185dd76f61d863
Jon Haddad <jon@jonhaddad.com>
2018-08-25 11:30:18 -0700

    packaging, getting so close

--- src/packaging/deb/build_deb.sh
+++ src/packaging/deb/build_deb.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-mkdir -p /deb/usr/{bin,lib}
-mkdir -p /deb/etc
-cp build/libs/tlp-stress-1.0-SNAPSHOT.jar /deb/usr/lib/
-cp bin/tlp-stress /deb/usr/bin/
+DEB_ROOT="build/deb"
 
-fpm -s dir -t deb -n tlp-stress -C /deb -m 'Jon Haddad' .
-chown
+# create hierarchy
+rm -rf "$DEB_ROOT"
+rm -rf *.deb
+
+mkdir -p "$DEB_ROOT/usr/local/bin"
+mkdir -p "$DEB_ROOT/usr/local/lib"
+mkdir -p "$DEB_ROOT/etc/tlp-stress"
+
+cp build/libs/tlp-stress-1.0-SNAPSHOT.jar "$DEB_ROOT/usr/local/lib/"
+cp bin/tlp-stress "$DEB_ROOT/usr/local/bin/"
+cp conf/logback-minimal.xml "$DEB_ROOT/etc/tlp-stress/logback.xml"
+
+sed -i '5s/.*/TLP_STRESS_JAR="\/usr\/local\/lib\/tlp-stress-1.0-SNAPSHOT.jar"/' "$DEB_ROOT/usr/local/bin/tlp-stress"
+
+fpm -s dir -t deb -n tlp-stress -C $DEB_ROOT -m 'Jon Haddad' -d openjdk-8-jre .
