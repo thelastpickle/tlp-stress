1aac4d2d58268a05f53268176228eba02b6d7735
Jon Haddad <jon@jonhaddad.com>
2018-10-29 16:06:29 -0700

    updating manual with real examples and tool to generate them

---
+++ manual/generate_examples.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+print_shell() {
+    # params
+    # $1 = name
+    # #2 = command
+
+    echo "running $1"
+
+    printf "$ %s\n" "$2" > manual/examples/"${1}.txt"
+    $2 >> manual/examples/"${1}.txt"
+}
+
+
+./gradlew assemble
+
+# help
+print_shell "tlp-stress-help" "bin/tlp-stress"
+
+# key value
+print_shell "tlp-stress-keyvalue" "bin/tlp-stress run KeyValue -n 10000"
+
+# info
+print_shell "info-key-value" "bin/tlp-stress info KeyValue"
+
+
+# list all workloads
+print_shell "list-all" "bin/tlp-stress list"
--- manual/generate_help.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-./gradlew assemble
-
-bin/tlp-stress > manual/tlp-stress-help.txt
