19288a159a6cc01411a1e166976e316365f78e22
Jon Haddad <jon@jonhaddad.com>
2019-03-01 16:10:46 -0800

    added prometheus metrics http server

--- src/main/kotlin/com/thelastpickle/tlpstress/Metrics.kt
+++ src/main/kotlin/com/thelastpickle/tlpstress/Metrics.kt
@@ -1,15 +1,29 @@
 package com.thelastpickle.tlpstress
 
-import com.codahale.metrics.MetricRegistry
-import com.codahale.metrics.ScheduledReporter
 import java.util.concurrent.TimeUnit
 
+import com.codahale.metrics.MetricRegistry
+import com.codahale.metrics.ScheduledReporter
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.dropwizard.DropwizardExports;
+import io.prometheus.client.exporter.HTTPServer
+
 class Metrics(metricRegistry: MetricRegistry, val reporters: List<ScheduledReporter>) {
+
+    val server: HTTPServer
 
     fun startReporting() {
         for(reporter in reporters)
             reporter.start(3, TimeUnit.SECONDS)
     }
+
+    init {
+        CollectorRegistry.defaultRegistry.register(DropwizardExports(metricRegistry))
+        server = HTTPServer(9500)
+
+    }
+
 
     val errors = metricRegistry.meter("errors")
     val mutations = metricRegistry.timer("mutations")
