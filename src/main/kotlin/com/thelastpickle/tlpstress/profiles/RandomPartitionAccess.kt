bae4aa555eeffd492be33b4bc8ddd5e2d3520ec4
Jon Haddad <jon@jonhaddad.com>
2019-06-19 15:47:57 -0700

    random partition reads and writes workload

---
+++ src/main/kotlin/com/thelastpickle/tlpstress/profiles/RandomPartitionAccess.kt
@@ -0,0 +1,90 @@
+package com.thelastpickle.tlpstress.profiles
+
+import com.datastax.driver.core.PreparedStatement
+import com.datastax.driver.core.Session
+import com.thelastpickle.tlpstress.PartitionKey
+import com.thelastpickle.tlpstress.StressContext
+import com.thelastpickle.tlpstress.WorkloadParameter
+import com.thelastpickle.tlpstress.generators.Field
+import com.thelastpickle.tlpstress.generators.FieldFactory
+import com.thelastpickle.tlpstress.generators.FieldGenerator
+import com.thelastpickle.tlpstress.generators.functions.Random
+import java.util.concurrent.ThreadLocalRandom
+
+class RandomPartitionAccess : IStressProfile {
+
+    @WorkloadParameter(description = "Number of rows per partition.")
+    var rows = 100
+
+    @WorkloadParameter("Select random row or the entire partition.  Acceptable values: row, partition")
+    var select = "row"
+
+    lateinit var insert : PreparedStatement
+    lateinit var query : PreparedStatement
+
+    override fun prepare(session: Session) {
+        insert = session.prepare("INSERT INTO random_access (partition_id, row_id, value) values (?, ?, ?)")
+
+        query = when(select) {
+
+            "partition" ->
+                session.prepare("SELECT * from random_access WHERE partition_id = ?")
+            "row" ->
+                session.prepare("SELECT * from random_access WHERE partition_id = ? and row_id = ?")
+            else ->
+                throw RuntimeException("select must be row or partition.")
+
+        }
+    }
+
+    override fun schema(): List<String> {
+        return listOf("""CREATE TABLE IF NOT EXISTS random_access (
+                           partition_id text,
+                           row_id int,
+                           value text,
+                           primary key (partition_id, row_id)
+                        )""")
+    }
+
+    override fun getRunner(context: StressContext) : IStressRunner {
+
+        println("Using $rows rows per partition")
+
+        return object : IStressRunner {
+
+            val value = context.registry.getGenerator("random_access", "value")
+            val random = ThreadLocalRandom.current()
+
+
+            override fun getNextMutation(partitionKey: PartitionKey): Operation {
+                val rowId = random.nextInt(0, rows)
+                val bound = insert.bind(partitionKey.getText(),
+                    rowId, value.getText())
+                return Operation.Mutation(bound)
+            }
+
+            override fun getNextSelect(partitionKey: PartitionKey): Operation {
+                val bound = when(select) {
+
+                    "partition" ->
+                        query.bind(partitionKey.getText())
+                    "row" -> {
+                        val rowId = random.nextInt(0, rows)
+                        query.bind(partitionKey.getText(), rowId)
+                    }
+                    else -> throw RuntimeException("not even sure how you got here")
+
+                }
+                return Operation.SelectStatement(bound)
+
+            }
+
+        }
+    }
+
+    override fun getFieldGenerators(): Map<Field, FieldGenerator> {
+        val ra = FieldFactory("random_access")
+        return mapOf(ra.getField("value") to Random())
+    }
+
+}
